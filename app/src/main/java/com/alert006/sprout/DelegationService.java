7079d78db00c86f19035479b8a79f33d5321adf5
Sprout Dev <dev@sprout.local>
2026-07-06 17:33:50 +0530

    Add asset links

---
+++ app/src/main/java/com/alert006/sprout/DelegationService.java
@@ -0,0 +1,14 @@
+package com.alert006.sprout;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
